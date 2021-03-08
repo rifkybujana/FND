5c1e8620211bb3028d2149a2793d53cac4746ab5
rifkybujana <rifkybujanabisri@gmail.com>
2021-03-08 20:57:25 +0700

    Setup pkg

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="FND",
+    version="0.0.1",
+    description="Fake News Detection AI",
+    license="GPL v3.0",
+    author="Rifky Bujana Bisri",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
