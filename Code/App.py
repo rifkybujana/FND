cbe03c0f180160e6fb6eae870dc8c0342f20d273
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2022-08-03 02:46:06 +0700

    Update App.py

--- Code/App.py
+++ Code/App.py
@@ -1,38 +1,39 @@
-from msilib import sequence
 import streamlit as st
 import numpy as np
+import pandas as pd
 import re
 import time
+import os
 
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer
+from transformers import AutoModelForSequenceClassification, AutoModel, AutoTokenizer
+from sklearn.metrics.pairwise import cosine_similarity
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
 from Scraper import Scrap
 
 st.set_page_config(layout="wide")
 
-model_checkpoint = "Rifky/FND"
+model_checkpoint = "Rifky/indobert-hoax-classification"
+base_model_checkpoint = "indobenchmark/indobert-base-p1"
+data_checkpoint = "Rifky/turnbackhoax-encoded"
 label = {0: "valid", 1: "fake"}
-
 
 @st.cache(show_spinner=False, allow_output_mutation=True)
 def load_model():
     model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=2)
+    base_model = SentenceTransformer(base_model_checkpoint)
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, fast=True)
-    return Trainer(model=model), tokenizer
-
+    data = load_dataset(data_checkpoint, split="train")
+    return model, base_model, tokenizer, data
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-
-def relu(x):
-    return np.maximum(0, x)
-
-
-input_column, reference_column = st.columns(2, gap="medium")
+input_column, reference_column = st.columns(2)
 input_column.write('# Fake News Detection AI')
 
 with st.spinner("Loading Model..."):
-    model, tokenizer = load_model()
+    model, base_model, tokenizer, data = load_model()
 
 user_input = input_column.text_input("Article url")
 submit = input_column.button("submit")
@@ -41,34 +42,46 @@ submit = input_column.button("submit")
 if submit:
     last_time = time.time()
     with st.spinner("Reading Article..."):
-        if user_input:
-            if user_input[:4] == 'http':
-                text = Scrap(user_input)
-            else:
-                text = user_input
+        title, text = Scrap(user_input)
 
     if text:
         text = re.sub(r'\n', ' ', text)
 
         with st.spinner("Computing..."):
-            text = text.split()
-            text_len = len(text)
+            token = text.split()
+            text_len = len(token)
 
             sequences = []
             for i in range(text_len // 512):
-                sequences.append(" ".join(text[i * 512: (i + 1) * 512]))
-            sequences.append(" ".join(text[text_len - (text_len % 512) : text_len]))
-            sequences = [tokenizer(i, max_length=512, truncation=True, padding="max_length") for i in sequences]
+                sequences.append(" ".join(token[i * 512: (i + 1) * 512]))
+            sequences.append(" ".join(token[text_len - (text_len % 512) : text_len]))
+            sequences = tokenizer(sequences, max_length=512, truncation=True, padding="max_length", return_tensors='pt')
 
-            predictions = model.predict(sequences)[0]
+            predictions = model(**sequences)[0].detach().numpy()
             result = [
                 np.sum([sigmoid(i[0]) for i in predictions]) / len(predictions), 
                 np.sum([sigmoid(i[1]) for i in predictions]) / len(predictions)
             ]
                 
             print (f'\nresult: {result}')
+            
+            title_embeddings = base_model.encode(title)
+            similarity_score = cosine_similarity(
+                [title_embeddings],
+                data["embeddings"]
+            ).flatten()
+            sorted = np.argsort(similarity_score)[::-1].tolist()
+            
             input_column.markdown(f"<small>Compute Finished in {int(time.time() - last_time)} seconds</small>", unsafe_allow_html=True)
             prediction = np.argmax(result, axis=-1)
             input_column.success(f"This news is {label[prediction]}.")
-            st.text(f"{int(result[prediction]*100)}% confidence")
+            input_column.text(f"{int(result[prediction]*100)}% confidence")
             input_column.progress(result[prediction])
+            
+            for i in sorted[:5]:
+                reference_column.write(f"""
+                <small>turnbackhoax.id</small>
+                <a href={data["url"][i]}><h5>{data["title"][i]}</h5></a>
+                """, unsafe_allow_html=True)
+                with reference_column.expander("read content"):
+                    st.write(data["text"][i])
