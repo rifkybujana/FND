66084a31a865454d24102f9af047d6ead2aafaf3
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2022-10-31 15:26:25 -0700

    Update Scraper.py

--- Code/Scraper.py
+++ Code/Scraper.py
@@ -3,11 +3,9 @@ from newspaper import Article
 def Scrap(url):
     """
     Scrap article from url
-
     ### Parameter\n
     url : article url (dtype: `string`)\n
     summarize : do you want to summarize the article? (dtype: `boolean`)
-
     ### Result\n
     return the article text (dtype: `string`)
     """
@@ -16,7 +14,7 @@ def Scrap(url):
     article.download()
     article.parse()
 
-    if not article.text:
+    if not article.text or not article.title:
         print("Can't Scrap this article link")
         return None
 
